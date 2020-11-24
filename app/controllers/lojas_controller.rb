25e7f7f10946c117dc8c1abf65321e78d9ab45d0
Fernando Valejo <terra.valejo@gmail.com>
2020-11-24 00:31:42 -0400

    iniciando configuração do layout

--- app/controllers/lojas_controller.rb
+++ app/controllers/lojas_controller.rb
@@ -1,7 +1,7 @@
 class LojasController < ApplicationController
 
   def index
-    @lojas = Loja.include(:transacao).select()
+    @lojas = Loja.join("left join transacoes t on t.loja_id = loja.id").select("*, sum(t.valor)")
   end
 
 end
